bc7c42741375ab08425ae290ea1ffeceefe56dda
hhhxnnnahhh <hanna@DESKTOP-Q78AMHN>
2025-12-12 00:24:11 +0200

    commit

--- BinomialHeap/BinomialHeap.cpp
+++ BinomialHeap/BinomialHeap.cpp
@@ -1,4 +1,4 @@
-#include "binomial_heap.hpp"
+#include "binomialheap.hpp"
 #include <iostream>
 using namespace std;
 
@@ -18,147 +18,254 @@ BinomialHeap<B>::BinomialHeap(const BinomialHeap<B>& other) {
 //BinomialHeap<B>::~BinomialHeap() {
 //	clear();
 //}
-///*
-// * insert
-// * 		Creates a singleton heap node with value v and
-// * 		 unions it with the head
-// * 	value:
-// * 		value to insert into heap
-// *
-// */
-//template <typename B>
-//void BinomialHeap<B>::insert(B value) {
-//
-//}
-///*
-// * getMin
-// * 		Searches the heap list for the node with minimum value
-// *
-// * returns
-// * 		minimum value in heap list
-// */
-//template <typename B>
-//B BinomialHeap<B>::getMin() {
-//
-//}
-///*
-// * extractMin
-// * 		Removes the heap list for node with minimum value
-// * 		and unions minimum node's children with heap list
-// *
-// * returns
-// * 		minimum value in heap list
-// */
-//template <typename B>
-//B BinomialHeap<B>::extractMin() {
-//
-//}
-///*
-// * deleteKey
-// * 		Removes the node with the value v by decreasing
-// * 		node value to one less than current min and then
-// * 		extracts the min.
-// * value:
-// * 		value to delete from heap
-// *
-// * returns
-// * 		if key not found throws exception
-// */
-//template <typename B>
-//void BinomialHeap<B>::deleteKey(B value) {
-//
-//}
-///*
-// * decreaseKey
-// * 		Searches for node with value v a
-// *
-// * 	value:
-// * 		value to find and decrease
-// * 	newValue:
-// * 		value to decrease value to
-// * returns
-// * 		if key not found throws exception
-// */
-//template <typename B>
-//void BinomialHeap<B>::decreaseKey(B value, B newValue) {
-//
-//}
-///*
-// * size
-// * 		Number of nodes in heap.
-// *
-// * returns
-// *		number of nodes in heap
-// */
-//template <typename B>
-//int BinomialHeap<B>::size() {
-//
-//}
-///*
-// * size
-// * 		Checks if empty
-// *
-// * returns
-// *		if empty
-// */
-//template <typename B>
-//bool BinomialHeap<B>::isEmpty() {
-//	retuen(head == 0);
-//}
-///*
-// * clear
-// * 		Removes all elements
-// *
-// * returns
-// *		none
-// */
-//template <typename B>
-//void BinomialHeap<B>::clear() {
-//	delete head;
-//	head = nullptr;
-//}
-//
-//template <typename B>
-//BinomialNode<B>* BinomialHeap<B>::unionHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB) {
-//
-//}
-///* mergeHeap
-// * 		Merges two heaps into a single Binomial heap by adding
-// * 		all siblings of heapA and heapB into a single heap containing
-// * 		all nodes in heapA and heapB.
-// * 		Assumes heapA and heapB siblings are ordered by root order(sibling count)
-// * 		Consolidating two heaps maintains merged ordering
-// *
-// * 	HeapA - root of heap to union with heapB
-// * 	HeapB - root of heap to union with heapA
-// *
-// * 	returns
-// * 		the root of the new heap that contains all heapA and heapB nodes
-// *
-// */
-//template <typename B>
-//BinomialNode<B>* BinomialHeap<B>::mergeHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB) {
-//
-//}
-///*
-// * order
-// * 		Number of children incident on heap
-// * 	heap
-// * 		pointer to root of Binomial heap
-// * 	returns
-// * 		order or heap
-//*/
-//template <typename B>
-//int BinomialHeap<B>::order(BinomialNode<B>* heap) {
-//
-//}
-
-// Binomial Node
 
 template <typename B>
+void BinomialHeap<B>::insert(B value) {
+	BinomialNode<B>* newNode = new BinomialNode<B>(value);
+	head = unionHeap(newNode, head);
+}
+template <typename B>
+B BinomialHeap<B>::getMin() {
+	if (head == nullptr) {
+		throw std::runtime_error("Cannot get min from empty heap");
+	}
+
+	B min = head->value;
+	BinomialNode<B>* currentPtr = head, * nextPtr = nullptr;
+	while (currentPtr != nullptr) {
+		if (currentPtr->value < min) {
+			min = currentPtr->value;
+		}
+		nextPtr = currentPtr->sibling;
+		currentPtr = nextPtr;
+	}
+	return min;
+}
+template <typename B>
+B BinomialHeap<B>::extractMin() {
+	if (head == nullptr) {
+		throw std::runtime_error("Cannot extract min from empty heap");
+	}
+	BinomialNode<B>* minPtr = head, * currentPtr = head, * prevPtr = nullptr, * minPrev = nullptr;
+	B minVal = head->value;
+	while (currentPtr != nullptr) {
+		if (currentPtr->value < minVal) {
+			minVal = currentPtr->value;
+			minPrev = prevPtr;
+			minPtr = currentPtr;
+		}
+		prevPtr = currentPtr;
+		currentPtr = currentPtr->sibling;
+	}
+	if (minPrev != nullptr) {
+		minPrev->sibling = minPtr->sibling;
+	}
+	else {
+		head = minPtr->sibling;
+	}
+	BinomialNode<B>* child = minPtr->child; // (akeno prevPtr belzabt) el leftmost child bta3 el heap (3ashan el heap lama bn delete el head bta3ha btb2a heaps soghayara bas ma3kosa)
+	BinomialNode<B>* newHead = nullptr; //3ayez el child el awalany yb2a el sibling bta3o null
+	while (child != nullptr) {
+		BinomialNode<B>* next = child->sibling;
+		child->sibling = newHead; //betkhaly el child yeshawer 3ala ely ablo (newHead)
+		newHead = child; //prevPtr = currentPtr
+		child = next; //currentPtr = nextPtr
+	}
+	head = unionHeap(head, newHead);
+	return minVal;
+}
+ ///*
+ // * deleteKey
+ // * 		Removes the node with the value v by decreasing
+ // * 		node value to one less than current min and then
+ // * 		extracts the min.
+ // * value:
+ // * 		value to delete from heap
+ // *
+ // * returns
+ // * 		if key not found throws exception
+ // */
+ //template <typename B>
+ //void BinomialHeap<B>::deleteKey(B value) {
+ //
+ //}
+
+template <typename B>
+void BinomialHeap<B>::decreaseKey(B value, B newValue) {
+	BinomialNode<B>* node = findKey(value);
+
+	if (!node) {
+		throw std::runtime_error("Key not found in heap");
+	}
+
+	if (newValue >= node->value) {
+		throw std::runtime_error("New value must be smaller than current value");
+	}
+
+	node->value = newValue;
+
+	BinomialNode<B>* current = node;
+	BinomialNode<B>* parent = current->parent;
+
+	while (parent && current->value < parent->value) {
+		// Swap 
+		B temp = current->value;
+		current->value = parent->value;
+		parent->value = temp;
+
+		current = parent;
+		parent = current->parent;
+	}
+}
+ //template <typename B>
+ //int BinomialHeap<B>::size() {
+ //
+ //}
+ ///*
+ // * size
+ // * 		Checks if empty
+ // *
+ // * returns
+ // *		if empty
+ // */
+ //template <typename B>
+ //bool BinomialHeap<B>::isEmpty() {
+ //	retuen(head == 0);
+ //}
+ ///*
+ // * clear
+ // * 		Removes all elements
+ // *
+ // * returns
+ // *		none
+ // */
+ //template <typename B>
+ //void BinomialHeap<B>::clear() {
+ //	delete head;
+ //	head = nullptr;
+ //}
+ //
+template <typename B>
+BinomialNode<B>* BinomialHeap<B>::unionHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB) {
+	BinomialNode<B>* heapU = mergeHeap(heapA, heapB);
+	if (heapU) {
+		BinomialNode<B>* curr = heapU, * prev, * next;
+		prev = nullptr;
+		next = nullptr;
+		while (curr && curr->sibling != nullptr) {
+			next = curr->sibling;
+			int orderA, orderB;
+			orderA = curr->order;
+			orderB = next->order;
+			if (orderA == orderB && orderB != order(next->sibling)) {
+				if (curr->value < next->value) {
+
+					curr->sibling = next->sibling;
+					curr->addChild(next);
+					prev = curr;
+					curr = curr->sibling;
+				}
+				else {
+
+					if (prev) {
+						prev->sibling = next;
+					}
+					else {
+						heapU = next;
+					}
+					next->addChild(curr);
+					prev = next;
+					curr = next->sibling;
+				}
+			}
+			else {
+
+				if (!prev) {
+					heapU = curr;
+				}
+
+				prev = curr;
+				curr = next;
+			}
+		}
+	}
+	return heapU;
+}
+template <typename B>
+BinomialNode<B>* BinomialHeap<B>::mergeHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB) {
+	if (!heapA) return heapB;
+	if (!heapB) return heapA;
+	BinomialNode<B>* head = nullptr;
+	BinomialNode<B>* tail = nullptr;
+	if (heapA->order <= heapB->order) {
+		head = heapA;
+		tail = heapA;
+		heapA = heapA->sibling;
+	}
+	else {
+		head = heapB;
+		tail = heapB;
+		heapB = heapB->sibling;
+	}
+	while (heapA && heapB) {
+		if (heapA->order <= heapB->order) {
+			tail->sibling = heapA;
+			tail = heapA;
+			heapA = heapA->sibling;
+		}
+		else {
+			tail->sibling = heapB;
+			tail = heapB;
+			heapB = heapB->sibling;
+		}
+	}
+	if (heapA) {
+		tail->sibling = heapA;
+	}
+	else if (heapB) {
+		tail->sibling = heapB;
+	}
+	return head;
+
+}
+template <typename B>
+BinomialNode<B>* BinomialHeap<B>::findKey(B value) {
+	if (!head) {
+		return nullptr;
+	}
+	BinomialNode<B>* currentRoot = head;
+
+	while (currentRoot) {  
+		BinomialNode<B>* foundNode = currentRoot->find(value);
+
+		if (foundNode) {
+			return foundNode;
+		}
+		currentRoot = currentRoot->sibling;
+	}
+	return nullptr;
+}
+template <typename B>
+int BinomialHeap<B>::order(BinomialNode<B>* heap) {
+	if (heap == nullptr) {
+		return -1;
+	}
+	int degree = 0;
+	BinomialNode<B>* child = heap->child;
+
+	while (child != nullptr) {
+		degree++;
+		child = child->sibling;
+	}
+
+	return degree;
+}
+
+// Binomial Node
+template <typename B>
 BinomialNode<B>::BinomialNode(B v, BinomialNode<B>* p) {
-	if(p != nullptr){
-		if(v < p->value){
+	if (p != nullptr) {
+		if (v < p->value) {
 			cout << "Error: child value less than parent value!(constructor)" << endl;
 			return;
 		}
@@ -170,17 +277,15 @@ BinomialNode<B>::BinomialNode(B v, BinomialNode<B>* p) {
 	sibling = nullptr;
 
 }
-
-
 template <typename B>
 void BinomialNode<B>::addChild(BinomialNode<B>* c) {
 	if (c != nullptr) {
-		if(c->value < value){
+		if (c->value < value) {
 			cout << "Error: child value less than parent value!(addChild func)" << endl;
 			return;
 		}
 		if (child == nullptr) {
-			
+
 			child = c;
 		}
 		else {
@@ -196,10 +301,24 @@ void BinomialNode<B>::addChild(BinomialNode<B>* c) {
 		order++;
 	}
 }
-//template <typename B>
-//BinomialNode<B>* BinomialNode<B>::find(B v) {
-//
-//}
+template <typename T>
+BinomialNode<T>* BinomialNode<T>::find(T v) {
+	if (value == v) {
+		return this;  
+	}
+	BinomialNode<T>* childPtr = child;  
+	while (childPtr) {  
+		// Recursively search this child's entire subtree
+		BinomialNode<T>* found = childPtr->find(v);
+
+		if (found) {
+			return found; 
+		}
+		childPtr = childPtr->sibling;
+	}
+	return nullptr; 
+}
+
 //template <typename B>
 //int BinomialNode<B>::size(int& i) {
 //
--- BinomialHeap/binomialheap.hpp
+++ BinomialHeap/binomialheap.hpp
@@ -13,128 +13,61 @@ class BinomialHeap
 {
 
 public:
-	
+
 	friend class BinomialNode<B>;
 
 	BinomialHeap();
 	BinomialHeap(const BinomialHeap& other);
 	//BinomialHeap& operator=(const BinomialHeap& other);
-	~BinomialHeap();
-	/*
-	 * insert
-	 * 		Creates a singleton heap node with value v and
-	 * 		 unions it with the head
-	 * 	value:
-	 * 		value to insert into heap
-	 *
-	 */
-	//void insert(B value);
-	///*
-	// * getMin
-	// * 		Searches the heap list for the node with minimum value
-	// *
-	// * returns
-	// * 		minimum value in heap list
-	// */
-	//B getMin();
-	///*
-	// * extractMin
-	// * 		Removes the heap list for node with minimum value
-	// * 		and unions minimum node's children with heap list
-	// *
-	// * returns
-	// * 		minimum value in heap list
-	// */
-	//B extractMin();
-	///*
-	// * deleteKey
-	// * 		Removes the node with the value v by decreasing
-	// * 		node value to one less than current min and then
-	// * 		extracts the min.
-	// * value:
-	// * 		value to delete from heap
-	// *
-	// * returns
-	// * 		if key not found throws exception
-	// */
-	//void deleteKey(B value);
-	///*
-	// * decreaseKey
-	// * 		Searches for node with value v a
-	// *
-	// * 	value:
-	// * 		value to find and decrease
-	// * 	newValue:
-	// * 		value to decrease value to
-	// * returns
-	// * 		if key not found throws exception
-	// */
-	//void decreaseKey(B value, B newValue);
-	///*
-	// * size
-	// * 		Number of nodes in heap.
-	// *
-	// * returns
-	// *		number of nodes in heap
-	// */
-	//int size();
-	///*
-	// * size
-	// * 		Checks if empty
-	// *
-	// * returns
-	// *		if empty
-	// */
-	//bool isEmpty();
-	/*
-	 * clear
-	 * 		Removes all elements
-	 *
-	 * returns
-	 *		none
-	 */
-	//void clear();
+	//~BinomialHeap();
+
+	void insert(B value);
+	B getMin();
+	B extractMin();
+	 ///*
+	 // * deleteKey
+	 // * 		Removes the node with the value v by decreasing
+	 // * 		node value to one less than current min and then
+	 // * 		extracts the min.
+	 // * value:
+	 // * 		value to delete from heap
+	 // *
+	 // * returns
+	 // * 		if key not found throws exception
+	 // */
+	 //void deleteKey(B value);
+	void decreaseKey(B value, B newValue);
+	 ///*
+	 // * size
+	 // * 		Number of nodes in heap.
+	 // *
+	 // * returns
+	 // *		number of nodes in heap
+	 // */
+	 //int size();
+	 ///*
+	 // * size
+	 // * 		Checks if empty
+	 // *
+	 // * returns
+	 // *		if empty
+	 // */
+	 //bool isEmpty();
+	 /*
+	  * clear
+	  * 		Removes all elements
+	  *
+	  * returns
+	  *		none
+	  */
+	  //void clear();
 protected:
-	/*Utility heap functions*/
-	/*
-	 * findKey
-	 * 		Searches heap for node with value equal to key.
-	 * 		v : value in heap to find
-	 * 	returns
-	 * 	 a pointer to node with value key
-	 */
-	//static BinomialNode<B>* unionHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB);
-	///* mergeHeap
-	// * 		Merges two heaps into a single Binomial heap by adding
-	// * 		all siblings of heapA and heapB into a single heap containing
-	// * 		all nodes in heapA and heapB.
-	// * 		Assumes heapA and heapB siblings are ordered by root order(sibling count)
-	// * 		Consolidating two heaps maintains merged ordering
-	// *
-	// * 	HeapA - root of heap to union with heapB
-	// * 	HeapB - root of heap to union with heapA
-	// *
-	// * 	returns
-	// * 		the root of the new heap that contains all heapA and heapB nodes
-	// *
-	// */
-	//static BinomialNode<B>* mergeHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB);
-	///*
-	// * order
-	// * 		Number of children incident on heap
-	// * 	heap
-	// * 		pointer to root of Binomial heap
-	// * 	returns
-	// * 		order or heap
-	//*/
-	//static int order(BinomialNode<B>* heap);
+	static BinomialNode<B>* unionHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB);
+	static BinomialNode<B>* mergeHeap(BinomialNode<B>* heapA, BinomialNode<B>* heapB);
+	BinomialNode<B>* findKey(B value);
+	static int order(BinomialNode<B>* heap);
 
 private:
-	/*
-	 * head
-	 * 		pointer to the head of the heap list where every
-	 * 		node is the root node of a heap
-	 */
 	BinomialNode<B>* head;
 };
 
@@ -146,7 +79,7 @@ public:
 	BinomialNode(B v, BinomialNode<B>* p = nullptr);
 
 	void addChild(BinomialNode<B>* child);
-	//BinomialNode<B>* find(B v);
+	BinomialNode<B>* find(B v);
 	//int size(int& i);
 	////used to append all nodes visited in this data structure into the other
 	//void addTo(BinomialHeap<B>* other);
